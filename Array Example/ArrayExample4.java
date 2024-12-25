a2ba923dd40e135d1846880642af2be4c44a8ce8
Bikram Kumal <92908935+bikutech44@users.noreply.github.com>
2024-12-25 12:44:25 +0545

    Add files via upload

---
+++ Array Example/ArrayExample4.java
@@ -0,0 +1,22 @@
+// Short array element in Ascending Order
+
+import java.util.Arrays; // use to short numbers in ascending order
+
+public class ArrayExample4 {
+    public static void main(String[] args) {
+        int[] numbers = {4, 2, 3, 1, 8, 6};
+
+        System.out.println("Original Array:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        // Sort the array in ascending order
+        Arrays.sort(numbers);
+
+        System.out.println("\nArray in Ascending Order:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
