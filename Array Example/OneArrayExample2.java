56fc251b57d7932e169bfb8556e291dfb46f256d
Bikram Kumal <92908935+bikutech44@users.noreply.github.com>
2024-12-25 10:06:02 +0545

    Add files via upload

---
+++ Array Example/OneArrayExample2.java
@@ -0,0 +1,17 @@
+// Program to find Array Largest Element
+
+public class OneArrayExample2 {
+    public static void main(String[] args) {
+        // declare and initialize the number
+        int[] numbers = {50, 20 , 11, 25, 40, 90, 80, 15};
+
+        // find Largest number
+        int largeNum = numbers[0];
+        for(int i = 1 ; i < (numbers.length); i++ ){
+            if(numbers[i]> largeNum){
+                largeNum = numbers[i];
+            }
+        }
+        System.out.println("Largest number is: "+ largeNum);
+    }
+}
