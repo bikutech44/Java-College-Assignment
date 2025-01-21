0d2bbeff245cf58cd3d2052918bc1b8119e3dcfd
Bikram Kumal <92908935+bikutech44@users.noreply.github.com>
2025-01-21 07:32:27 +0545

    Add files via upload

---
+++ Inheritance Example/InheritanceExample3InterfaceExample.java
@@ -0,0 +1,24 @@
+interface Shape {
+    void draw(); // Abstract method
+}
+
+class Circle implements Shape {
+    public void draw() {
+        System.out.println("Drawing a circle.");
+    }
+}
+
+class Square implements Shape {
+    public void draw() {
+        System.out.println("Drawing a square.");
+    }
+}
+public class InheritanceExample3InterfaceExample {
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        Shape square = new Square();
+
+        circle.draw();
+        square.draw();
+    }
+}
