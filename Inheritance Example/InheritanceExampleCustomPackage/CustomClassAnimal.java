6e4710e0bc13a74ecb9429ea7d3cfcf84aa5af52
Bikram Kumal <92908935+bikutech44@users.noreply.github.com>
2025-01-21 07:33:55 +0545

    Add files via upload

---
+++ Inheritance Example/InheritanceExampleCustomPackage/CustomClassAnimal.java
@@ -0,0 +1,9 @@
+package InheritanceExampleCustomPackage;
+
+
+public class CustomClassAnimal {
+    public void sound(){
+        System.out.println("Animals make sounds.");
+    }
+
+}
---
+++ Inheritance Example/InheritanceExampleCustomPackage/CustomClassDog.java
@@ -0,0 +1,7 @@
+package InheritanceExampleCustomPackage;
+
+public class CustomClassDog extends CustomClassAnimal{
+    public void sound() {
+        System.out.println("Dog barks.");
+    }
+}
