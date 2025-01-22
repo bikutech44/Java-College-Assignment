35588599be4bcb5c045ae13a51eb79d802060e63
Bikram Kumal <92908935+bikutech44@users.noreply.github.com>
2025-01-22 21:37:00 +0545

    Add files via upload

---
+++ Array Example/ArrayExample1FindSum.java
@@ -0,0 +1,15 @@
+// Program to Find sum of Array Element
+
+public class ArrayExample1FindSum {
+    public static void main(String[] args) {
+        // Declare and initialize array number
+        int[] numbers = {10, 20, 30, 40, 50, 60, 70};
+
+        // calculate sum of the array elements.
+        int sum = 0;
+        for(int i : numbers){
+            sum += i;
+        }
+        System.out.println("Sum of the array number is: "+ sum);
+    }
+}
